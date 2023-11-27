9367b99362f5b3b7ca8b1bb486a3b3f55ec9ac25
Robert Azarcon <azarcon3@illinois.edu>
2023-11-27 15:43:52 -0600

    docker build file modified

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 
-docker build -f $DIR/Dockerfile -t ece484 ..
+docker build -f $DIR/Dockerfile -t codaero/ece484 ..
